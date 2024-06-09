19240100c56a70349c25ec8972a80d3054086ef3
mohammedmuzzammiluddin <165559209+mohammedmuzzammiluddin@users.noreply.github.com>
2024-06-09 15:34:22 +0530

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -10,16 +10,16 @@ root.title("Sapphire")  # Add the title "Sapphire"
 def play_gif():
     root.lift()
     root.attributes("-topmost", True)
-    global img
-    img = Image.open("original-3b009b0c0dccfa3891b49158bb3dbd81")  # Enter the gif address
+    global vid
+    vid = vid.open("original-3b009b0c0dccfa3891b49158bb3dbd81")  # Enter the gif address
     lbl = Label(root)
     lbl.place(x=0, y=0)
     i = 0
 
-    for img in ImageSequence.Iterator(img):
-        img = img.resize((1000, 500))
-        img = ImageTk.PhotoImage(img)
-        lbl.config(image=img)
+    for vid in ImageSequence.Iterator(vid):
+        vid = vid.resize((1000, 500))
+        vid = ImageTk.PhotoImage(vid)
+        lbl.config(video=vid)
         root.update()
         time.sleep()
 
